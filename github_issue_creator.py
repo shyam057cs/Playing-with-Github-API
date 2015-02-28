a52c22fbec954aa2f7cdf86bf094cf20e26656cb
Shyam Shankar <syamsankar91@gmail.com>
2015-02-28 16:25:02 +0530

    Added script that will create issue in github

---
+++ github_issue_creator.py
@@ -0,0 +1,28 @@
+__author__ = 'Syam Sankar'
+
+import json
+from urllib2 import urlopen, Request, HTTPError
+
+MY_GITHUB_URL = "https://api.github.com/repos/shyam057cs/Playing-with-Github-API/issues"
+MY_TOKEN = "a85f1bc21ca136dd5da1b7892df75990bdf03caf"
+
+def create_issue(title,body,url = MY_GITHUB_URL, token = MY_TOKEN, ):
+    data = json.dumps({ "title": title, "body": body})
+
+    request = Request(url)
+    request.add_data(data)
+    request.add_header('Authorization', 'token %s' % token)
+
+    try:
+        response = urlopen(request).read()
+    except HTTPError, err:
+        print "Unable to create the issue. Error:" + err
+    else:
+        print 'Successfully created the issue.'
+
+        parsed = json.loads(response)
+        return json.dumps(parsed, indent=4, sort_keys=True)
+
+if __name__ == '__main__':
+    create_issue("new title issue", "new body")
+
