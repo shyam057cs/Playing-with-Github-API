a20be162de76f94bb051f3a8978bb7de9bd95f0c
Shyam Shankar <syamsankar91@gmail.com>
2015-03-04 15:57:00 +0530

    added feature for extracting email and creating issue

--- github_issue_creator.py
+++ github_issue_creator.py
@@ -3,8 +3,12 @@ __author__ = 'Syam Sankar'
 import json
 from urllib2 import urlopen, Request, HTTPError
 
+import poplib
+import re
+from email import parser
+
 MY_GITHUB_URL = "https://api.github.com/repos/shyam057cs/Playing-with-Github-API/issues"
-MY_TOKEN = "valid_token"
+MY_TOKEN = "valid token"
 
 def create_issue(title,body,url = MY_GITHUB_URL, token = MY_TOKEN, ):
     data = json.dumps({ "title": title, "body": body})
@@ -23,6 +27,29 @@ def create_issue(title,body,url = MY_GITHUB_URL, token = MY_TOKEN, ):
         parsed = json.loads(response)
         return json.dumps(parsed, indent=4, sort_keys=True)
 
+
+
+def main():
+    pop_conn = poplib.POP3_SSL('pop.gmail.com')
+    pop_conn.user('devil057cs')
+    pop_conn.pass_('plugin1234')
+
+    regex = re.compile("<([a-zA-Z]+).*?>(.*?)</\\1>")
+    #Get messages from server:
+    messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
+    # Concat message pieces:
+    messages = ["\n".join(mssg[1]) for mssg in messages]
+    #Parse message intom an email object:
+    messages = [parser.Parser().parsestr(mssg) for mssg in messages]
+    extracted_body = "Empty Description"
+    for message in messages:
+        for part in message.walk():
+            if part.get_content_type():
+                body = part.get_payload(decode=True)
+        extracted_body=  regex.findall(body)[0][1]
+        create_issue(message['subject'], extracted_body)
+    pop_conn.quit()
+
 if __name__ == '__main__':
-    create_issue("new title issue", "new body")
+    main()
 
